8d5c566563991f6375da8085f8a1e860bbef6f87
cvped <stay488@gmail.com>
2024-11-29 18:44:11 +0900

    [신재경] Memo 프로젝트

---
+++ src/main/java/com/cvped/memo/user/UserController.java
@@ -0,0 +1,21 @@
+package com.cvped.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+	@GetMapping("/user/join-view")
+	public String inputJoin() {
+		return "user/join";
+	}
+	
+	
+	@GetMapping("/user/login-view")
+	public String inputLogin(){
+		return "/user/login";
+	}
+}
